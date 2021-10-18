8648d175c56a7478edfedad0dece2c1bf07e5dd9
mubassari <maulimubassari23@gmail.com>
2021-10-18 19:33:05 +0800

    Move to Vue CLI

--- js/preload.js
+++
@@ -1,8 +0,0 @@
-// Preload
-$(window).on('load', function () {
-    $('.spinner').fadeOut();
-    $('.preloader').delay(350).fadeOut('slow');
-    $('body').delay(350).css({
-        'overflow': 'visible'
-    });
-});
